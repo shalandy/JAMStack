321610876cdbeea38e9b6029c6a75572fe1b582d
shalandy <shalandy@berkeley.edu>
2018-09-23 12:47:01 -0700

    gatsby init

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>This is my JAMStack Hackathon Application!</p>
+  </Layout>
+)
+
+export default IndexPage
